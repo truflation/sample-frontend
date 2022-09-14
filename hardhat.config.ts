d3fa1181638d9438499b67692f994560ad6a2088
Joseph C Wang <joequant@gmail.com>
2022-09-14 12:22:17 +0800

    add operator contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+	version: "0.8.17"
+      },
+      {
+	version: "0.7.6"
+      }
+    ]
+  }
 };
 
 export default config;
